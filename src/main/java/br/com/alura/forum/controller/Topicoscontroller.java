1f8d118e29e5a714da1cd179a795f737d289650e
luislima <lggugs@gmail.com>
2019-09-24 20:47:52 -0300

    ordenar resultados por um campo

--- src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
+++ src/main/java/br/com/alura/forum/controller/Topicoscontroller.java
@@ -23,6 +23,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -51,8 +52,9 @@ public class Topicoscontroller {
     
     @GetMapping
     public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso,
-            @RequestParam int pagina, @RequestParam int quantidade){ 
-        Pageable paginacao = PageRequest.of(pagina, quantidade);
+            @RequestParam int pagina, @RequestParam int quantidade,
+            @RequestParam String ordenacao){ 
+        Pageable paginacao = PageRequest.of(pagina, quantidade, Sort.Direction.DESC, ordenacao);
         if (null == nomeCurso){            
             Page<Topico> topicos = topicoRepository.findAll(paginacao);
             return TopicoDto.converter(topicos);
