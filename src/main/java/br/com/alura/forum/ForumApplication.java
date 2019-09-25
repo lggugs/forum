1204d3e42ce2d5710aee0a2836e21d5d3bf8dd37
luislima <lggugs@gmail.com>
2019-09-24 21:04:26 -0300

    Habilitar módulo web para paginação automática via request

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport // Habilita o suporte ao uso de paginação via request 
 public class ForumApplication {
 
 	public static void main(String[] args) {
